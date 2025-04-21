f59949dd2050ba8447a13f41a4bb8e8955117a95
Marcin Czaniecki <m.czaniecki@arex.pl>
2025-04-21 14:42:39 +0200

    Add priority and status levels helper files

---
+++ lib/modules/issues/helpers/priorityLevels.ts
@@ -0,0 +1,22 @@
+export const priorityLevels = [
+  {
+    value: 3,
+    name: "Niski",
+  },
+  {
+    value: 4,
+    name: "Normalny",
+  },
+  {
+    value: 5,
+    name: "Wysoki",
+  },
+  {
+    value: 6,
+    name: "Pilny",
+  },
+  {
+    value: 7,
+    name: "Natychmiastowy",
+  },
+];
---
+++ lib/modules/issues/helpers/statusLevels.ts
@@ -0,0 +1,34 @@
+export const statusLevels = [
+  {
+    value: 4,
+    name: "Brak dantch",
+  },
+  {
+    value: 2,
+    name: "W toku",
+  },
+  {
+    value: 10,
+    name: "Do poprawy",
+  },
+  {
+    value: 9,
+    name: "Do testów",
+  },
+  {
+    value: 7,
+    name: "W testach",
+  },
+  {
+    value: 39,
+    name: "Wykonane",
+  },
+  {
+    value: 5,
+    name: "Zamknięte",
+  },
+  {
+    value: 6,
+    name: "Anulowane (zamknięte)",
+  },
+];
