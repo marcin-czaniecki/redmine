7b72bba0e852c940983dd18a1e12a3cfe0203b2c
Marcin Czaniecki <m.czaniecki@arex.pl>
2025-04-23 10:19:05 +0200

    Refactor add_issue.ts to improve project retrieval logic and enhance error handling for better clarity and maintainability

--- lib/modules/issues/core/add_issue.ts
+++ lib/modules/issues/core/add_issue.ts
@@ -7,82 +7,90 @@ import { RedmineProject } from "../../../types/RedmineProject.js";
 import { priorityLevels } from "../helpers/priorityLevels.js";
 import { RedmineIssue } from "../../../types/RedmineIssue.js";
 
-const redmineUrl = await getRedmineUrl();
-
-async function getAllProjects(): Promise<RedmineProject[]> {
-  const url = `${redmineUrl}/projects.json`;
-  let offset = 0;
-  const limit = 100;
-  let total_count = 0;
-  let allProjects: RedmineProject[] = [];
-
-  do {
-    const { data } = await getRedmine<{ projects: RedmineProject[]; total_count: number; offset: number; limit: number }>(url, {
-      params: { limit, offset },
-    });
-    allProjects = allProjects.concat(data.projects);
-    total_count = data.total_count;
-    offset += limit;
-  } while (allProjects.length < total_count);
-
-  return allProjects;
-}
-
-const projects = await getAllProjects();
-
-const project: RedmineProject = await arg({
-  placeholder: "Wybierz projekt",
-  choices: projects.map((project) => {
-    return {
-      name: `[${project.id}] ${project.name}`,
-      value: project,
-      description: project.description,
-    };
-  }),
-});
-
-const project_id = project.id;
-
-const {
-  data: { memberships },
-} = await getRedmine<{ memberships: RedmineMembership[] }>(`${redmineUrl}/projects/${project_id}/memberships.json`, {});
-
-const subject = await arg("Wprowadź tytuł");
-const description = await editor({
-  language: "md",
-  footer: `Wprowadź opis`,
-});
-
-const priority_id = await arg({
-  placeholder: "Wybierz priorytet",
-  choices: priorityLevels,
-});
-
-const assigned_to_id = await arg({
-  placeholder: "Wybierz priorytet",
-  choices: memberships.map((membership) => {
-    return {
-      name: membership.user.name,
-      value: membership.user.id,
-    };
-  }),
-});
-
 try {
-  const result = await postRedmine<{ issue: RedmineIssue }>(`${redmineUrl}/issues.json`, {
-    issue: {
-      tracker_id: 18,
-      project_id,
-      subject,
-      description,
-      priority_id,
-      assigned_to_id,
-    },
+  const redmineUrl = await getRedmineUrl();
+
+  async function getAllProjects(): Promise<RedmineProject[]> {
+    const url = `${redmineUrl}/projects.json`;
+    let offset = 0;
+    const limit = 100;
+    let total_count = 0;
+    let allProjects: RedmineProject[] = [];
+
+    do {
+      const { data } = await getRedmine<{ projects: RedmineProject[]; total_count: number; offset: number; limit: number }>(url, {
+        params: { limit, offset },
+      });
+      allProjects = allProjects.concat(data.projects);
+      total_count = data.total_count;
+      offset += limit;
+    } while (allProjects.length < total_count);
+
+    return allProjects;
+  }
+
+  const projects = await getAllProjects();
+
+  const project: RedmineProject = await arg({
+    placeholder: "Wybierz projekt",
+    choices: projects.map((project) => {
+      return {
+        name: `[${project.id}] ${project.name}`,
+        value: project,
+        description: project.description,
+      };
+    }),
   });
 
-  if (result.status === 201) {
-    await run("./read_issue.ts", result.data.issue.id.toString());
+  const project_id = project.id;
+
+  const {
+    data: { memberships },
+  } = await getRedmine<{ memberships: RedmineMembership[] }>(`${redmineUrl}/projects/${project_id}/memberships.json`, {});
+
+  const subject = await arg("Wprowadź tytuł");
+  const description = await editor({
+    language: "md",
+    footer: `Wprowadź opis`,
+  });
+
+  const priority_id = await arg({
+    placeholder: "Wybierz priorytet",
+    choices: priorityLevels,
+  });
+
+  const assigned_to_id = await arg({
+    placeholder: "Wybierz priorytet",
+    choices: memberships.map((membership) => {
+      return {
+        name: membership?.user?.name || "unknown",
+        value: membership?.user?.id || "unknown",
+      };
+    }),
+  });
+
+  try {
+    const result = await postRedmine<{ issue: RedmineIssue }>(`${redmineUrl}/issues.json`, {
+      issue: {
+        tracker_id: 18,
+        project_id,
+        subject,
+        description,
+        priority_id,
+        assigned_to_id,
+      },
+    });
+
+    if (result.status === 201) {
+      await run("./read_issue.ts", result.data.issue.id.toString());
+    }
+  } catch (error) {
+    await div(md(`# Wystąpił problem:\n\n ${error?.message}`));
   }
 } catch (error) {
-  await div(md(`# Wystąpił problem:\n\n ${error?.message}`));
+  if (error instanceof Error) {
+    await div(JSON.stringify(error.message));
+  } else {
+    await div(JSON.stringify(error));
+  }
 }
